6f2cbc97c8e5e6301840c7a62aad579292e6758c
natlownes <nat.lownes@gmail.com>
2010-04-25 16:28:04 -0400

    hex

--- lib/models/player.rb
+++ lib/models/player.rb
@@ -7,9 +7,8 @@ class Player < Person
   index :rs_id
   
   def to_hex_color
-    digest = Digest::MD5.hexdigest(self.name)
-    digest = digest[0..5]
-    # digest.unpack('U' * digest.length).collect {|x| x.to_s 16}
+    digest = Digest::MD5.hexdigest(self.rs_id)
+    digest[0..5]
   end
   
 end
